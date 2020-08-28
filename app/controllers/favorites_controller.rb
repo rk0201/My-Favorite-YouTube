2fafc609b49445035a823ea4dca96341c1a30fc9
rk0201 <ryuryuryu54100201@gmail.com>
2020-08-28 09:27:25 +0000

    favorite index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
+  def index
+    @articles = Article.joins(:favorites).where(favorites: { user_id: params[:user_id] })
+  end
+  
   def create
     @favorite = current_user.favorites.create(article_id: params[:article_id])
     redirect_back(fallback_location: root_path)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  resources :users
+  resources :users do
+    resources :favorites
+  end
+  
   resources :articles do
     resource :favorites, only: [:create, :destroy]
   end
