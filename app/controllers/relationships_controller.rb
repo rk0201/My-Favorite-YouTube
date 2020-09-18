81ecea2e1172e751eaaed10064b2746c5e926663
EC2 Default User <ec2-user@ip-172-31-2-186.us-east-2.compute.internal>
2020-09-18 10:43:54 +0000

    20200918

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,5 @@
-class RelationshipsController < ApplicationController
+class RelationshipsController < 
+  before_action :authenticate_user!
     def create
       @user = User.find(params[:following_id])
       current_user.follow(@user)
